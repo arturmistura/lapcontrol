0ad4ad241c942408c1f2d091b52a53114c9fab66
Artur José Mistura Junior <arturmistura@gmail.com>
2018-04-18 22:40:55 -0300

    função de equipe praticamente pronta

--- src/app/equipe/equipe.component.ts
+++ src/app/equipe/equipe.component.ts
@@ -3,34 +3,43 @@ import { NgForm } from '@angular/forms';
 import { Equipe } from '../models/equipe';
 
 @Component({
-  selector: 'app-equipe',
-  templateUrl: './equipe.component.html',
-  styleUrls: ['./equipe.component.css']
+	selector: 'app-equipe',
+	templateUrl: './equipe.component.html',
+	styleUrls: ['./equipe.component.css']
 })
 export class EquipeComponent implements OnInit {
 
-  equipes: Equipe[];
-  equipe: Equipe;
-  count = 0;
+	equipes: Equipe[];
+	equipe: Equipe;
+	count = 0;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-    this.equipes = new Array<Equipe>();
-    this.equipe = new Equipe();
-  }
+	ngOnInit() {
+		this.equipes = new Array<Equipe>();
+		this.equipe = new Equipe();
+	}
 
-  salvarEquipe() {
-    if (this.equipe.id == null || this.equipe.id == 0) {
-      console.log(this.equipe);
-      this.equipe.id = this.count++;
-      this.equipes.push(this.equipe);
-    }
+	salvarEquipe() {
+		if (this.equipe.id == null || this.equipe.id == 0) {
+			this.equipe.id = ++this.count;
+			this.equipes.push(this.equipe);
+		} else {
+			var index = this.equipes.findIndex(a => a.id == this.equipe.id);
+			this.equipes[index] = this.equipe;
+		}
+		
+		this.equipe = new Equipe();
+	}
 
-    this.equipe = new Equipe();
-  }
+	editarEquipe(equipe: Equipe) {
+		Object.assign(this.equipe, equipe);
+	}
 
-  editarEquipe(equipe: Equipe) {
-    this.equipe = equipe;
-  }
+	excluirEquipe(equipe: Equipe) {
+		if(equipe.id > 0){
+			let index = this.equipes.findIndex(a => a.id == equipe.id);
+			this.equipes.splice(index, 1);
+		}
+	}
 }
